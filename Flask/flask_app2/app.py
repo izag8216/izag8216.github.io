2046d35b05f90beca790a9ea149adf0b7e971dae
ItoMaru <31996215+izag8216@users.noreply.github.com>
2021-08-20 19:47:56 +0800

    Add files via upload

--- Flask/flask_app2/app.py
+++ Flask/flask_app2/app.py
@@ -16,22 +16,6 @@ class BlogPost(db.Model):
     def __repr__(self):
         return 'Blog post ' + str(self.id)
 
-all_posts = [
-    {
-        'title': 'Post 1',
-        'content': 'This is the content of post 1.',
-        'rates': 'Good!',
-        'date': '20210819',
-        'author': 'Itomaru'
-    },
-    {
-        'title': 'Post 2',
-        'content': 'This is the content of post 2.',
-        'rates': 'Not Good!',
-        'date': '20210817'
-    },
-]
-
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -42,7 +26,8 @@ def posts():
     if request.method == 'POST':
         post_title = request.form['title']
         post_content = request.form['content']
-        new_post = BlogPost(title=post_title, content=post_content, author='Itomaru')
+        post_author = request.form['author']
+        new_post = BlogPost(title=post_title, content=post_content, author=post_author)
         db.session.add(new_post)
         db.session.commit()
         return redirect('/posts')
@@ -61,6 +46,41 @@ def hello(name, id):
 @app.route('/onlyget', methods=['GET'])
 def get_req():
     return 'You can only get this webpage.'
+
+@app.route('/posts/delete/<int:id>')
+def delete(id):
+    post = BlogPost.query.get_or_404(id)
+    db.session.delete(post)
+    db.session.commit()
+    return redirect('/posts')
+
+@app.route('/posts/edit/<int:id>', methods=['GET','POST'])
+def edit(id):
+    
+    post = BlogPost.query.get_or_404(id)
+    
+    if request.method == 'POST':
+        post.title=request.form['title']
+        post.author=request.form['author']
+        post.content=request.form['content']
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        return render_template('edit.html',post=post)
+
+@app.route('/posts/new', methods=['GET','POST'])
+def new_post():
+    if request.method == 'POST':
+        post.title=request.form['title']
+        post.author=request.form['author']
+        post.content=request.form['content']
+        new_post = BlogPost(title=post_title, content=post_content, author=post_author)
+        db.session.add(new_post)
+        db.session.commit()
+        return redirect('/posts')
+    else:
+        return render_template('new_post.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
