645d4c6606bc687222b9bab763881f2fbef20214
ItoMaru <31996215+izag8216@users.noreply.github.com>
2021-09-06 11:33:50 +0800

    Add files via upload

---
+++ ML365/D47_365/whatsapp.py
@@ -0,0 +1,10 @@
+from typing import Counter
+import regex
+import pandas as pd
+import numpy as np
+import emoji
+from collections import Counter
+import matplotlib.pyplot as plt
+%matplotlib inline
+from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
+
---
+++ ML365/D49_365/Fairy-tale-creation-model-main/README.md
@@ -0,0 +1,2 @@
+# Fairy-tale-creation-model
+If you give the opening sentence of a fairy tale, it is a model that creates the continuation of the story.
